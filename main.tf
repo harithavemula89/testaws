84c7fc74bcba64e060da50451f6a5132bd807bdb
harithavemula89 <harithavemula89@gmail.com>
2024-05-26 18:33:03 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-west-2" # Update to your desired region
+}
+
+# Define a variable for the cluster name
+variable "cluster_name" {
+  description = "The name of the ECS Cluster"
+  default     = "my-ecs-cluster"
+}
+
+# Create the ECS Cluster
+resource "aws_ecs_cluster" "my_cluster" {
+  name = var.cluster_name
+}
+
+# Create an IAM role for ECS task execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}

82e64fbf124b9ca5a46c4d4a18c5c5bf62a60908
harithavemula89 <harithavemula89@gmail.com>
2024-05-27 17:27:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,9 @@ resource "aws_iam_role" "ecs_task_execution_role" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action    = "sts:AssumeRole",
+        "Action": [
+                "ecs:CreateCluster",
+                "ecs:ListClusters",
         Effect    = "Allow",
         Principal = {
           Service = "ecs-tasks.amazonaws.com"

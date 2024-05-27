13694abf437d11895368debd2a490d388b786803
harithavemula89 <harithavemula89@gmail.com>
2024-05-27 17:37:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,19 +15,43 @@ resource "aws_ecs_cluster" "my_cluster1" {
 
 # Create an IAM role for ECS task execution
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRolepolicy"
+  name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        "Action": [
-                "ecs:CreateCluster",
-                "ecs:ListClusters"
-        Effect    = "Allow",
+        Effect = "Allow",
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
-        }
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach the AmazonECSTaskExecutionRolePolicy policy to the IAM role
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Create an inline policy for ecs:CreateCluster and ecs:ListClusters actions
+resource "aws_iam_role_policy" "ecs_custom_policy" {
+  name   = "ecsCustomPolicy"
+  role   = aws_iam_role.ecs_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecs:CreateCluster",
+          "ecs:ListClusters"
+        ],
+        Resource = ["*"]
       }
     ]
   })
